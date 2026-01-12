f1437b69ab36ffe5eb378b6eb54af39a9fd08512
chickendrop89 <chickendrop89@gmail.com>
2026-01-12 22:17:58 +0100

    tree: add license comment to more files

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#  Sync external monitor brightness with daylight cycles via DDC/CI.
+#  Copyright (C) 2026 chickendrop89
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
 echo "Building ..."
 echo "Make sure you have dbus daemon, and mutter-devkit installed" 
 echo 
